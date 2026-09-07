ab5ae2111862530086fb3780528999b4d91cfcd4
IgorSilva2 <boxigor13@gmail.com>
2026-09-07 14:25:59 +0100

    Att

--- Projeto CPP/Projeto_forca/main.cpp
+++ Projeto CPP/Projeto_forca/main.cpp
@@ -1,23 +1,60 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <clocale>
 
 using namespace std;
 
-void Nome_usuario(string Nome)
+void Dificuldade(string a)
 {
-    cin >> Nome;
-    cout << "\nNome: " << Nome;
+    int Escolha;
+    cout << "Escolha a dificulade : ";
+    cin >> Escolha;
+    switch (Escolha)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+
+    }
+}
+void Salva_nome(string a)
+{
+    ofstream ficheiro("Nome_jogadores.txt", ios::app);
+    if(ficheiro.is_open())
+    {
+        ficheiro << a << "\n";
+    }
+    ficheiro.close();
 }
 
 int main()
 {
-    string usuario;
-    cout << "***************************" << endl;
-    cout << "*      JOGO DA FORCA      *" << endl;
-    cout << "***************************" << endl;
+    setlocale(LC_ALL, "Portuguese");
+    string Nome;
+    int Escolha;
 
-    cout << "digite o seu nome : ";
-    Nome_usuario(usuario);
+    do
+    {
+        cout << "***************************" << endl;
+        cout << "*      JOGO DA FORCA      *" << endl;
+        cout << "***************************" << endl;
+
+        cout << "digite o seu nome : ";
+        cin >> Nome;
+
+        Salva_nome(Nome);
+
+
+        cout << "Dezeja repetir ? 1 = Sim - 0 = N�o : ";
+        cin >> Escolha;
+    }
+    while(Escolha != 0);
+
+
 
 
 
