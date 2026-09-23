727bec90eca70bf1876c99cfbe65c660856d94a7
IgorSilva2 <boxigor13@gmail.com>
2026-09-23 15:47:34 +0100

    Atualizando

--- Programaçao curso/Linguagem/Java/Exercicios.java
+++ Programaçao curso/Linguagem/Java/Exercicios.java
@@ -662,3 +662,17 @@ public class Empregado {
     }
 }
 
+ /*Atividade 5 – Herança, Polimorfismo e Encapsulamento
+
+1. Escreva um programa em JAVA que permita guardar os dados de funcionários externos e internos
+da empresa:
+Nota: Faça uso das principais propriedades da POO, tais como, Herança, Polimorfismo e
+Encapsulamento, usando-as adequadamente.
+a. Crie uma classe Pessoa com métodos chamados getPrimeiroNome() e getUltimoNome()
+e os atributos genéricos de uma pessoa.
+b. Crie uma subclasse chamada funcInterno que adicione um novo método chamado
+getIdEmpregado() e substitua o método geUltimoNome() para incluir também o cargo do
+funcionário.
+c. Crie uma classe que descreva um funcionário externo (funcExterno) que contenha apenas
+os atributos código e tipo de serviço prestado. */
+
